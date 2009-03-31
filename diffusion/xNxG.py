68db631747c12f1cc7fedabf0ac95ad813f14bcd
Dave Williams <cdave@u.washington.edu>
2009-03-31 14:54:47 -0700

    Diffusion: added xNxG start

--- diffusion/xNxG.py
+++ diffusion/xNxG.py
@@ -1,18 +1,26 @@
-## This file defines the system for the xNCG crossbridge. This 
+## This file defines the system for the xNxG crossbridge. This 
 ## crossbridge type has linear springs representing the neck and
-## globular regions, and a torsional spring representing the 
-## converter domain.
+## globular regions. These springs are angled such that they replicate
+## a version of the one-dimensional model. There is some ambiguity as 
+## to how we represent the one-dimensional model in a two-dimensional 
+## space, with no principle present to guide use between the choices 
+## of having a globular domain spring with no stiffness (allowing the 
+## pseudo-one-dimensional model to range freely over all lattice 
+## spacings) or having a globular domain with infinite stiffness 
+## (fixing the head at one particular lattice spacing). I am leaning 
+## towards the all-lattice spacings interpretation to indicate that the
+## one-dimensional crossbridge is independent of lattice spacing, but the 
+## fixed-spacing version does accuretly conveigh that the one-dimensional
+## crossbridge is simply incapable of looking at lattice spacings.
 ##      H   - Head of the myosin
 ##      |  
-##      G   - Globular domain, linear spring
+##      G   - Globular domain, linear spring/fixed length
 ##      |  
-##      C   - Converter region, torsional spring
+##      C   - Converter region, fixed angle
 ##     /   
 ##    N     - Neck region, linear spring
 ##   /     
-##========= - Thick filament
-
-# FIXME: Find correct scaling factor for kT to be used with our pN forces and nM scales
+##==T====== - Thick filament, fixed angle
 
 
 from numpy import array, pi, sin, cos, tan, arctan2, sqrt, hypot
@@ -20,27 +28,25 @@ import numpy as np
 from scipy.optimize import fmin_powell as fmin
 import time
 import contour
-#import graphXB
+import graphXB
 
 
-class xNCG():
+class xNxG():
     def __init__(self):
         """Create the values we'll be referencing for the XB"""
-        self.Ts = pi/4 # angle (rad) of the connection to the thick fil
+        self.Ts = 0 # angle (rad) of the connection to the thick fil
         self.Tk = 100  # spring constant of connection to the thick fil
         self.Ns = 5    # rest length of neck region
         self.Nk = 10    # spring constant of neck region
-        self.Nv = (6, 6, 6) # normal and rigor values of Ns
-        self.Cs = pi/3+(pi-self.Ts) #rest angle (rad) of the converter domain
+        self.Cs = pi/2+(pi-self.Ts) #rest angle (rad) of the converter domain
         self.Ck = 100  # torsional spring const of converter domain
         self.Cv = (pi/3, pi/3, 1.2*pi/3) # normal and rigor values of Cs
         self.Gs = 3    # rest length of globular domain
-        self.Gk = 5    # spring constant of globular domain
+        self.Gk = 0    # spring constant of globular domain
         self.Gv = (5, 5, 5) # normal and rigor values of Gs
         self.Bd = 0.55 # dist at which binding becomes likely
         # Current state and identity of XB
-        self.rest_conv_loc() # Sets conv_loc to rest location
-        self.rest_head_loc() # Sets head_loc to rest location
+        self.rest_conv_and_head_loc() # Set conv_loc and head_loc to rest locs
         self.bound = False
         self.state = 0 # 0 is unbound, 1 is loosely, 2 is strongly
         # Diffusion related values
@@ -57,20 +63,22 @@ class xNCG():
         # Angles and lengths
         T = self.thic_ang()
         N = self.neck_len()
-        G = self.glob_len()
         C = self.conv_ang()
+        G = self.glob_len()
         # Energies
         Tu = 0.5 * self.Tk * (T-self.Ts)**2
         Nu = 0.5 * self.Nk * (N-self.Ns)**2 
         Cu = 0.5 * self.Ck * (C-self.Cs)**2
         Gu = 0.5 * self.Gk * (G-self.Gs)**2
+        Total = Tu + Nu + Cu + Gu
         return ("Angles/Lengths and Energies\n" +
                 "===========================\n" +
                 "x = ang/len : energy\n" +
-                "T = %02.3fpi : %02.3f (fixed)\n" %(T/pi, Tu) +
+                "T = %02.3fpi : %02.3f fixed \n" %(T/pi, Tu) +
                 "N = %02.3f   : %02.3f \n" %(N, Nu) + 
-                "C = %02.3fpi : %02.3f \n" %(C/pi, Cu) + 
-                "G = %02.3f   : %02.3f" %(G, Gu))
+                "C = %02.3fpi : %02.3f fixed \n" %(C/pi, Cu) + 
+                "G = %02.3f   : %02.3f \n" %(G, Gu) +
+                "Tot energy  = %02.3f" %Total)
         
     def probability(self):
         """Given the location of the XB head,
@@ -89,38 +97,33 @@ class xNCG():
         """Set the conv_loc to minimize the XB's energy 
         and return the newly located minimum energy
         """
-        e = lambda (l): self.e_dep_conv(l)
-        min_n_len = fmin(e, self.Ns, disp=0)
+        (x, y) = self.head_loc
+        self.conv_loc = (x, self.conv_loc[1])
         return self.energy()
         
-    def e_dep_conv(self, N_len):
-        """Update the conv_loc and return the xb energy"""
-        N_len = float(N_len) # Was a numpy array, weirding TK
-        self.conv_loc = (N_len * cos(self.Ts),
-                         N_len * sin(self.Ts))
-        return self.energy()
-    
     def energy(self):
         """Return the energy of the xb without altering any positions"""
+        T = self.thic_ang()
         N = self.neck_len()
-        G = self.glob_len()
         C = self.conv_ang()
-        return (0.5 * self.Nk * (N-self.Ns)**2 +
+        G = self.glob_len()
+        return (0.5 * self.Tk * (T-self.Ts)**2 + 
+                0.5 * self.Nk * (N-self.Ns)**2 +
                 0.5 * self.Ck * (C-self.Cs)**2 + 
                 0.5 * self.Gk * (G-self.Gs)**2)
-    
+        
     def glob_len(self):
         """Return the globular length at the current head_loc"""
         x = self.head_loc[0] - self.conv_loc[0]
         y = self.head_loc[1] - self.conv_loc[1]
         return hypot(x, y)
-    
+        
     def conv_ang(self):
         """Return the converter angle at the current head_loc"""
         x = self.head_loc[0] - self.conv_loc[0]
         y = self.head_loc[1] - self.conv_loc[1]
         return arctan2(y, x) + pi - self.thic_ang()
-    
+        
     def neck_len(self):
         """Return the neck length at the current conv_loc"""
         return hypot(self.conv_loc[0], self.conv_loc[1])
@@ -130,50 +133,55 @@ class xNCG():
         x = self.conv_loc[0]
         y = self.conv_loc[1]
         return arctan2(y, x)
-    
-    def rest_conv_loc(self):
-        """Set the converter loc to its rest location"""
+        
+    def rest_conv_and_head_loc(self):
+        """Set the converter and head loc to their rest locations"""
         self.conv_loc = (self.Ns * cos(self.Ts),
                          self.Ns * sin(self.Ts))
-    
-    def rest_head_loc(self):
-        """Set the head loc to its rest location"""
         x = self.conv_loc[0] + self.Gs * cos(self.Cs + self.Ts - pi)
         y = self.conv_loc[1] + self.Gs * sin(self.Cs + self.Ts - pi)
         self.head_loc = (x, y)
 
-## b = xNCG()
+## b = xNxG()
 ## window = graphXB.graphXB()
-## window.draw_XB(b, free=(False, True, True, True))
+## window.draw_XB(b, free=(True, True, True, True))
 ## for i in np.arange(14,0,-.25):
-##     b.head_loc = (3.7, i)
+##     b.head_loc = (12-i, i)
 ##     b.minimize()
 ##     print(b)
 ##     window.update_XB(b)
-##     time.sleep(.1)
+##     time.sleep(.01)
+## for i in np.arange(14,0,-.25):
+##     b.head_loc = (4, i)
+##     b.minimize()
+##     print(b)
+##     window.update_XB(b)
+##     time.sleep(.01)
+## for i in np.arange(14,-3,-.25):
+##     b.head_loc = (i, 5)
+##     b.minimize()
+##     print(b)
+##     window.update_XB(b)
+##     time.sleep(.01)
 
-## Begin the script that will produce the matrix of stored probabilities
+# Begin the script that will produce the matrix of stored energies
 print("This might take a while")
 x_locs = np.arange(-3, 13, .1) 
 y_locs = np.arange(0, 16, .1)
-probs = np.zeros((y_locs.size, x_locs.size))
+energs = np.zeros((y_locs.size, x_locs.size))
 # Instantiate the xb
-xb = xNCG()
-# Cycle through and collect all the probabilities
+xb = xNxG()
+# Cycle through and collect all the energies
 n = [0,0]
 for y in y_locs:
     for x in x_locs:
         xb.head_loc = (x,y)
-        probs[n[0], n[1]] = xb.probability()
+        energs[n[0], n[1]] = xb.minimize()
         n[1] = n[1] + 1
     n[0] = n[0] + 1
     n[1] = 0
-# Normalize the probabilities
-min = np.min(probs)
-max = np.max(probs)
-probs = (probs - min)/(max-min)
-contour.title = "Probability of an xNCG crossbridge being\n found at a given head locations"
+contour.title = "Energy level of an xNxG crossbridge at different head locations"
 contour.xlabel = "Location of XB head (nm)"
 contour.ylabel = "Location of XB head (nm)"
-contour.levels = [.9, .95, .98, .99, .999] 
-contour.contour(x_locs, y_locs, probs)
+contour.levels = [1, 4, 7, 10, 13]
+contour.contour(x_locs, y_locs, energs)
